06e6d96d4229145c997ab0c81fd222c3187e04d2
Simon <simonsivek@seznam.cz>
2025-11-08 17:25:36 +0100

    Scraper works - version 1.0

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 from bs4 import BeautifulSoup
 import requests
 import tkinter
 from tkinter import ttk
+import os
+from dotenv import load_dotenv
 import datetime
 import lxml
 #twilio sms poslani
 
+load_dotenv()
+
+my_email = os.getenv("MY_EMAIL")
+password = os.getenv("PASSWORD")
+smtp_server = "smtp.gmail.com"
+smtp_port = 587
+if not my_email or not password:
+    print("Chyba: E-mailové údaje nebyly nalezeny v .env souboru.")
+    print("Ujistěte se, že máte soubor .env a obsahuje EMAIL_USER a EMAIL_PASS.")
+
+#future - object orianted 
 law_response = []
 
 #WINDOW SETUP
@@ -31,51 +46,22 @@ welcome_frame.pack(fill="x", pady=10)
 central_label = ttk.Label(welcome_frame, text="Law scraper", font=("Calibri", 30)).grid(column=1, row=0)
 ttk.Label(welcome_frame, text="Search for law by law number, year and paragraph", font=("Calibri", 20)).grid(column=1, row=1)
 
-# central_label = tkinter.Label(text="Law Scraper", font=("Calibri", 30))
-# central_label.grid(column=1, row=0)
-
-# explain_label = tkinter.Label(text="Type what law, year and paragraph you want to know", font=("Calibri", 20))
-# explain_label.grid(column=1, row=1)
-
 #LAW INPUT
 
 input_frame = ttk.Frame(window, padding=10)
 input_frame.pack(fill="x", pady=10)
 
 ttk.Label(input_frame, text="Law number").grid(column=0, row=2, pady=5)
-# law_label = tkinter.Label(text="Law", font=("Calibri", 12))
-# law_label.grid(column=0, row=2)
 law_number = ttk.Entry(input_frame, width=20)
 law_number.grid(column=0, row=3, padx=5, sticky="ew", pady=5)
-
-# law = tkinter.Entry(width=20)
-# law.focus
-# law.insert(index=0, string=0)
-# law.grid(column=0, row=3)
 
 ttk.Label(input_frame, text="Year").grid(column=1, row=2, pady=5)
 year = ttk.Entry(input_frame, width=20)
 year.grid(column=1, row=3, padx=5, pady=5)
 
-# year_label = tkinter.Label(text="Year", font=("Calibri", 12))
-# year_label.grid(column=1, row=2)
-
-# year = tkinter.Entry(width=20)
-# year.focus()
-# year.insert(index=0, string=0)
-# year.grid(column=1, row=3)
-
 ttk.Label(input_frame, text="Paragraph").grid(column=2, row=2, pady=5)
 paragraph = ttk.Entry(input_frame, width=20)
 paragraph.grid(column=2, row=3, padx=5, pady=5)
-
-# paragraph_label = tkinter.Label(text="Paragraph", font=("Calibri", 12))
-# paragraph_label.grid(column=2, row=2)
-
-# paragraph = tkinter.Entry(width=20)
-# paragraph.focus
-# law.insert(index=0, string=0)
-# paragraph.grid(column=2, row=3)
 
 # SEARCH FUNCTION/class
 
@@ -86,12 +72,20 @@ headers = {
 
 def get_law():
     global law_response
-    response = requests.get(url=f"https://krajta.slv.cz/2006/262/par_101", headers=headers)
+    p = paragraph.get()
+    y = year.get()
+    n = law_number.get()
+
+    if not p or not y or not n:
+        search_result.config(text="Vyplňte všechna pole!", foreground="red")
+        return
+
+    response = requests.get(url=f"https://krajta.slv.cz/{y}/{n}/par_{p}", headers=headers)
     response.raise_for_status()
     data = response.text
 
     soup = BeautifulSoup(data, "lxml")
-    selector = "[id*='par_101']"
+    selector = f"[id*='par_{p}']"
     law = soup.select(selector=selector)
     for i in law:
         law_response.append(i.get_text())
@@ -106,27 +100,36 @@ search_button.grid(column=1, row=4)
 
 #LAW SEARCH
 
-
 search_frame = ttk.Frame(window, padding=10)
 search_frame.pack(fill="x", pady=10)
 
-# search_button = tkinter.Button(text="Search", command=None)
-# search_button.grid(column=1, row=4)
+search_result = ttk.Label(search_frame, font=("Calibri", 15))
+search_result.grid(column=0, row=5)
 
-search_result = ttk.Label(search_frame, text="-", font=("Calibri", 15))
-# search_result = tkinter.Label(text="-", font=("Calibri", 15))
-search_result.grid(column=1, row=5)
-
-#email function 
-
+#EMAIL FUNCTION
 
 def sending_email():
-    my_email = "simonsivek@seznam.cz"
-    password = "abgb1747"
-    connection = smtplib.SMTP("smtp.seznam.cz")
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    # connection.sendmail(from_addr=my_email, to_addrs=email_input, msg=)
+    try:
+        if law_response == []:
+            email_sent_result.config(text="Nejprve vyhledej zákon", foreground="red")
+        email_to = email_input.get()
+        msg = MIMEMultipart()
+        msg["FROM"] = my_email
+        msg["TO"] = email_to
+        msg["SUBJECT"] = "Zákon, který jsi hledal!"
+        for i in law_response:
+            msg.attach(MIMEText(f"{i}\n", "plain"))
+
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            server.starttls()
+            server.login(my_email, password)
+            server.sendmail(from_addr=my_email, to_addrs=email_to, msg=msg.as_string())
+        
+        email_sent_result.config(text="Email BYL poslán!", foreground="green")
+        print("Email nebyl poslán!")
+    except Exception as e:
+        email_sent_result.config(text="Email NEBYL poslán!", foreground="red")
+        print(f"Email nebyl poslán! Error: {e}")
 
 
 #SENDING A MAIL
@@ -138,22 +141,16 @@ ttk.Label(email_frame, text="Send it to your email").grid(column=1, row=6, pady=
 email_input = ttk.Entry(email_frame, width=40)
 email_input.grid(column=1, row=7, pady=5)
 
-# email_input = tkinter.Entry(width=40)
-# email_input.focus
-# email_input.grid(column=1, row=6)
-
 email_button = ttk.Button(email_frame, text="Send", command=sending_email)
 email_button.grid(column=1, row=8)
 
-# email_button = tkinter.Button(text="Send", command=None)
-# email_button.grid(column=1, row=8)
+#EMAIL CONFIRMATION
 
-my_email = "simonsivek4@gmail.com"
-password = "abgb1747"
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-connection.login(user=my_email, password=password)
-connection.sendmail(from_addr=my_email, to_addrs=email_input)
+email_sent_frame = ttk.Frame(window, padding=10)
+email_sent_frame.pack(fill="x", pady=10)
+
+email_sent_result = ttk.Label(email_sent_frame, font=("Calibri", 15))
+email_sent_result.grid(column=1, row=5)
 
 #SENDING SMS
 
